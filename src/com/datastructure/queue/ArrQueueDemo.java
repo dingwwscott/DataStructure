55fbb824a7bb03ea02f7ea4e70eb309aa829e066
dingwwscott <857377712@qq.com>
2019-12-15 22:04:07 +0800

    对列

--- src/com/datastructure/queue/ArrQueueDemo.java
+++ src/com/datastructure/queue/ArrQueueDemo.java
@@ -4,10 +4,9 @@ public class ArrQueueDemo {
     public static void main(String[] args) {
 
     }
-
-    //使用数组模拟队列 编写一个ArrayQueue类
 }
 
+//使用数组模拟队列 编写一个ArrayQueue类
 class ArrayQueue{
     private int maxSize;  //数组的最大容量
     private int front;    //队列头
@@ -15,5 +14,11 @@ class ArrayQueue{
     private int[] arr;    //用于存放数据，模拟队列
 
     //创建队列的构造器
+    public ArrayQueue(int arrMaxSize){
+        maxSize = arrMaxSize;
+        arr = new int[arrMaxSize];
+        front = -1;  //指向队列头部，分析出front是指向队列头的前面一个位置
+        rear = -1;   //指向队列尾部，指向队列尾的数据(即就是队列最后一个数据)
+    }
 
 }
