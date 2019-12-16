b1d7e652e16b7777e0ec91053486ed0ae548bf0b
dingwwscott <857377712@qq.com>
2019-12-16 10:12:16 +0800

    对列

--- src/com/datastructure/queue/ArrQueueDemo.java
+++ src/com/datastructure/queue/ArrQueueDemo.java
@@ -21,4 +21,25 @@ class ArrayQueue{
         rear = -1;   //指向队列尾部，指向队列尾的数据(即就是队列最后一个数据)
     }
 
+    //判断队列是否满
+    public boolean isFull(){
+        return maxSize - 1 == rear;
+    }
+
+    //判断队列是否空
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    //添加数据到对列
+    public void addQueue(int number){
+        //判断队列是否满
+        if(isFull()){
+            System.out.println("队列已满");
+            return;
+        }
+        rear++;
+        arr[rear] = number;
+    }
+
 }
