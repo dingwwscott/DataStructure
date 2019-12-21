eeb4dea58fefe9df247e7fedd919a660ae585edc
dingwwscott <857377712@qq.com>
2019-12-21 10:27:54 +0800

    链表

---
+++ src/com/datastructure/linkedlist/SingleLinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.datastructure.linkedlist;
+
+public class SingleLinkedListDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+//定义HeroNode，每个 HeroNode 对象就是一个节点
+class HeroNode{
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode next;               //指向下一个节点
+}
