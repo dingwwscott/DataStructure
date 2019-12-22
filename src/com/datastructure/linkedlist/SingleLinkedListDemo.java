889acdd7ce9caa6b831475831447cb523fcb5a12
dingwwscott <857377712@qq.com>
2019-12-22 16:36:11 +0800

    单链表的增删改查

--- src/com/datastructure/linkedlist/SingleLinkedListDemo.java
+++ src/com/datastructure/linkedlist/SingleLinkedListDemo.java
@@ -5,21 +5,38 @@ public class SingleLinkedListDemo {
 
         //测试
         //先创建节点
-        HeroNode hero1 = new HeroNode(1,"宋江","及时雨");
-        HeroNode hero2 = new HeroNode(2,"庐俊毅","玉麒麟");
-        HeroNode hero3 = new HeroNode(3,"吴用","智多星");
-        HeroNode hero4 = new HeroNode(4,"林冲","豹子头");
+        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+        HeroNode hero2 = new HeroNode(2, "庐俊毅", "玉麒麟");
+        HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
 
         //创建链表
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         //加入
-        singleLinkedList.add(hero1);
-        singleLinkedList.add(hero2);
-        singleLinkedList.add(hero3);
-        singleLinkedList.add(hero4);
+//        singleLinkedList.add(hero1);
+//        singleLinkedList.add(hero2);
+//        singleLinkedList.add(hero3);
+//        singleLinkedList.add(hero4);
 
+        //按编号加入
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero3);
+        singleLinkedList.addByOrder(hero2);
+
+        //显示链表
         singleLinkedList.list();
 
+        //测试修改节点
+        HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟******");
+        singleLinkedList.update(newHeroNode);
+        System.out.println("修改节点后的链表");
+        singleLinkedList.list();
+
+        //删除一个节点
+        singleLinkedList.delete(1);
+        System.out.println("删除后的链表");
+        singleLinkedList.list();
     }
 }
 
@@ -41,6 +58,79 @@ class SingleLinkedList {
         }
 
         temp.next = heroNode;                 //当退出while循环时，temp就指向了链表的最后，将最后这个节点的next指向新的节点
+    }
+
+    //根据排名插到指定位置（如果有这个排名，则添加失败，并给出提示）
+    public void addByOrder(HeroNode heroNode) {
+        HeroNode temp = head;
+        boolean flag = false;                                        //flag添加的编号是否存在，默认为false
+        while (true) {
+            if (temp.next == null) {                                   //说明temp已经在链表的最后
+                break;
+            }
+            if (temp.next.no > heroNode.no) {                          //位置找到，就在temp的后面插入
+                break;
+            } else if (temp.next.no == heroNode.no) {                   //说明编号已经存在
+                flag = true;
+                break;
+            }
+            temp = temp.next;                                        //后移，继续遍历
+        }
+        if (flag) {
+            System.out.println(heroNode.no + "编号已经存在，不能插入");
+        } else {
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
+    //修改节点的信息，根据no编号来修改，即no编号不能改
+    public void update(HeroNode newHeroNode) {               //根据newHeroNode 的 no来修改
+
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+
+        HeroNode temp = head.next;
+        boolean flag = false;                               //表示是否找到该节点
+        while (true) {
+            if (temp == null) {
+                break;                                     //已经遍历完列表
+            }
+            if (temp.no == newHeroNode.no) {
+                flag = true;                               //找到
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {                                        //根据flag判断是否找到要修改的节点
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        } else {
+            System.out.println("没有找到编号为" + newHeroNode.no + "的节点");
+        }
+    }
+
+    //删除节点
+    public void delete(int no){
+        HeroNode temp = head;
+        boolean flag = false;                          //是否找到待删除节点
+        while (true){
+            if (temp.next == null){                    //已到链表的最后
+                break;
+            }
+            if(temp.next.no == no){
+                flag = true;                          //找到要删除节点的前一个节点
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag){
+            temp.next = temp.next.next;
+        }else {
+            System.out.println("要删除的节点" + no + "不存在");
+        }
     }
 
     //显示链表，遍历
